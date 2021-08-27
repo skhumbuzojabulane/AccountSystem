a60d26862d4c0c7f0302a2e04b8521a7cd120691
skhumbuzojabulane <73143829+skhumbuzojabulane@users.noreply.github.com>
2021-08-27 03:43:45 +0200

    comit

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/DemoController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/DemoController.java
@@ -1,12 +1,15 @@
 package za.ac.nwu.ac.web.sb.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//
+//import za.ac.nwu.ac.domain.service.GeneralResponse;
 
 @RestController
 public class DemoController
 {
-    //@GetMapping("/ping")
+    //@GetMapping("/all")
+    //public GeneralResponse<String> getAll()
+    {
+        //return new GeneralResponse<String>(successful: true, payload: "No types found");
+    }
 }
